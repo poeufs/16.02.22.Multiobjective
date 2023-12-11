9d8c9364fe5d952bddbbfe431b51e663690508c7
poeufs <91636297+poeufs@users.noreply.github.com>
2023-12-11 16:29:41 +0100

    hydropower disaggregation, Nobj

---
+++ Multiobjective-multi-reservoir-control-d50e4da0f6a9a9c852b4904e640299adc96714bb/ZambeziSmashPython/notebooks/Convergence_hydropower.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import numpy as np
+from ema_workbench import (MultiprocessingEvaluator, ema_logging, RealParameter, ScalarOutcome, Constant,
+                           Model, HypervolumeMetric, save_results)
+from ema_workbench.em_framework.optimization import (GenerationalBorg, epsilon_nondominated, to_problem, ArchiveLogger,
+                                                     EpsilonProgress)
+#################
+#### HYDROPOWER
+################
+
+if not os.path.exists("../src"):
+    os.chdir("../../src")
+else:
+    os.chdir('../src')
+os.getcwd()
+
+from model_zambezi_OPT_hyd import ModelZambezi
+
+ZambeziProblem = ModelZambezi()
+
+def model_wrapper(**kwargs):
+    input = [kwargs['v' + str(i)] for i in range(len(kwargs))]
+    Hydropower, Environment, Irrigation, HydropowerITT, HydropowerKGU, HydropowerKA, HydropowerCB, HydropowerKGL = tuple(ZambeziProblem.evaluate(np.array(input)))
+    return Hydropower, Environment, Irrigation, HydropowerITT, HydropowerKGU, HydropowerKA, HydropowerCB, HydropowerKGL
+# specify model
+model = Model('zambeziproblem', function=model_wrapper)
+
+# levers
+model.levers = [RealParameter('v' + str(i), -1, 1) for i in range(ZambeziProblem.Nvar)]
+
+# specify outcomes
+model.outcomes = [ScalarOutcome('Hydropower', ScalarOutcome.MINIMIZE),  # Minimize, because deficits
+                  ScalarOutcome('Environment', ScalarOutcome.MINIMIZE),
+                  ScalarOutcome('Irrigation', ScalarOutcome.MINIMIZE),
+                  ScalarOutcome('HydropowerITT', ScalarOutcome.MINIMIZE),
+                  ScalarOutcome('HydropowerKGU', ScalarOutcome.MINIMIZE),
+                  ScalarOutcome('HydropowerKA', ScalarOutcome.MINIMIZE),
+                  ScalarOutcome('HydropowerCB', ScalarOutcome.MINIMIZE),
+                  ScalarOutcome('HydropowerKGL', ScalarOutcome.MINIMIZE),
+                  ]
+
+#Problem definition
+problem = to_problem(model, searchover="levers")
+
+os.getcwd()
+
+# LOAD THE RUN
+nfe = 2  # 150000 1 seed; 35000 5 seeds in HPC
+seeds = 1  # 5
+run_comment = 'hyd_NOBJhyd'  # add a comment to recognize the run output
+run_label = f"{run_comment}_{nfe}nfe_{seeds}seed"
+
+os.chdir(f'../runs/{run_label}')
+os.getcwd()
+
+import tarfile
+def alternative_load_archives(filename): #cls,
+    archives = {}
+    with tarfile.open(os.path.abspath(filename)) as fh:
+        for entry in fh.getmembers():
+            if entry.name.endswith("csv"):
+                key = entry.name.split("/")[1][:-4]
+                archives[int(key)] = pd.read_csv(fh.extractfile(entry), index_col=0)
+    return archives
+
+ArchiveLogger.load_archives = alternative_load_archives
+
+convergences = []
+for i in range(seeds):
+    df = pd.read_csv(f"convergence{i}.csv")
+    convergences.append(df)
+    #f'convergence{i}' = pd.read_csv(f"{run_name}/convergence{i}.csv")
+
+# Load the archives
+all_archives = []
+for i in range(seeds):
+    archives = ArchiveLogger.load_archives(f"archives/{i}.csv")
+    # archives.items()[nfe] =
+    # archives = archives.loc[:, ~archives.columns.str.contains('^Unnamed')]
+    all_archives.append(archives)
+
+# Define the reference list
+reference_set = epsilon_nondominated(results_list, [0.05] * len(model.outcomes), problem)  # [0.05]
+
+#Define the hypervolumemetric
+hv = HypervolumeMetric(reference_set, problem)
+
+from datetime import datetime
+# Calculate the metrics
+before = datetime.now()
+print("time before is", before)
+
+metrics_by_seed = []
+for archives in all_archives:
+    metrics = []
+    for nfe, archive in archives.items():
+        scores = {
+            "hypervolume": hv.calculate(archive),
+            "nfe": int(nfe),
+        }
+        metrics.append(scores)
+    metrics = pd.DataFrame.from_dict(metrics)
+
+    # sort metrics by number of function evaluations
+    metrics.sort_values(by="nfe", inplace=True)
+
+    metrics_by_seed.append(metrics)
+
+after = datetime.now()
+print(f"It took {after - before} time to do metrics HYD")
+
+# Visualize convergence metrics
+sns.set_style("white")
+fig, axes = plt.subplots(nrows=2, figsize=(8, 12), sharex=True)
+
+ax1, ax2 = axes
+
+for metrics, convergence in zip(metrics_by_seed, convergences):
+    ax1.plot(metrics.nfe, metrics.hypervolume)
+    ax1.set_ylabel("hypervolume")
+
+    ax2.plot(convergence.nfe, convergence.epsilon_progress)
+    ax2.set_ylabel("$\epsilon$ progress")
+
+sns.despine(fig)
+
+plt.show()
