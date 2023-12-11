e4fc752583ea1e7b5916fc3cebfe3376326429fc
poeufs <91636297+poeufs@users.noreply.github.com>
2023-12-11 16:55:50 +0100

    hydropower disaggregation, Nobj

--- Multiobjective-multi-reservoir-control-d50e4da0f6a9a9c852b4904e640299adc96714bb/ZambeziSmashPython/notebooks/optimization_irr.py
+++ Multiobjective-multi-reservoir-control-d50e4da0f6a9a9c852b4904e640299adc96714bb/ZambeziSmashPython/notebooks/optimization_irr.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     ################################# RUN SETTINGS #######################################
     ######################################################################################
     # Specify the nfe and add a comment for the run save name
-    nfe = 150000 #150000 #35000
+    nfe = 100000 #150000 #35000
     seeds = 1 #5
     epsilon_list = [0.1] * len(model.outcomes) # Test values: [0.9] * len(model.outcomes), after observing base case:
     # [0.2, 0.5, 0.1], previous version's epsilons: [0.1] * len(model.outcomes)
