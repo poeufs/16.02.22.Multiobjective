390e35b415ea69e78c977581f3c846e553197586
poeufs <91636297+poeufs@users.noreply.github.com>
2024-02-08 00:52:01 +0100

    irrigation opt: nobj = 11 (data/settings and settings/settings_file). epsilon tests

--- Multiobjective-multi-reservoir-control-d50e4da0f6a9a9c852b4904e640299adc96714bb/ZambeziSmashPython/notebooks/optimization_irr.py
+++ Multiobjective-multi-reservoir-control-d50e4da0f6a9a9c852b4904e640299adc96714bb/ZambeziSmashPython/notebooks/optimization_irr.py
@@ -66,11 +66,11 @@ if __name__ == '__main__':
     ################################# RUN SETTINGS #######################################
     ######################################################################################
     # Specify the nfe and add a comment for the run save name
-    nfe = 1000 #150000 #35000
-    seeds = 3 #5
-    epsilon_list = [0.2, 0.5, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3] # Test values: [0.9] * len(model.outcomes), after observing base case:
+    nfe = 100000 #150000 #35000
+    seeds = 1 #5
+    epsilon_list = [0.3, 0.6, 0.4, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5] # Test values: [0.9] * len(model.outcomes), after observing base case:
     # , previous version's epsilons: [0.1] * len(model.outcomes)
-    run_comment = 'nobj3'  # add a comment to recognize the run output
+    run_comment = 'eps3645-5'  # add a comment to recognize the run output
     ######################################################################################
 
     run_label = f"IR_{run_comment}_{nfe}nfe_{seeds}seed" #IR = Irrigation (11objectives)
