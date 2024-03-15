37ffda56cf4054b0790bf71d9681c894fbcebf54
poeufs <91636297+poeufs@users.noreply.github.com>
2024-03-15 03:14:33 +0100

    simulation in progress

--- Multiobjective-multi-reservoir-control-d50e4da0f6a9a9c852b4904e640299adc96714bb/ZambeziSmashPython/src/various_plots.py
+++ Multiobjective-multi-reservoir-control-d50e4da0f6a9a9c852b4904e640299adc96714bb/ZambeziSmashPython/src/various_plots.py
@@ -85,7 +85,7 @@ def plot_quantities():
 
     # copy here..
     #####################################
-    feature = 'three_policy_simulation'
+    feature = 'bc_policy_simulation'
     # reservoirs=['itt','kgu','kgl','ka','bg','dg','cb','mn']
     reservoirs = ['itt', 'kgu', 'kgl', 'ka', 'cb']
     title = '5_res_wKGL'
@@ -99,6 +99,68 @@ def plot_quantities():
 
     policies = ['best_hydro', 'best_env', 'best_irr']
     irr_index = ['2', '3', '4', '5', '6', '7', '8', '9']
+    irr_d = ['Irrigation District 2', 'Irrigation District 3', 'Irrigation District 4', 'Irrigation District 5',
+             'Irrigation District 6', 'Irrigation District 7', 'Irrigation District 8', 'Irrigation District 9']
+    label_policy = ['Best Hydropower', 'Best Environment', 'Best Irrigation', 'Target Demand']
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    n_months = 12
+    n_years = 20
+    purple = '#7a0177';
+    yellow = '#fdaa09';
+    blue = '#1d91c0'  # green='#41ab5d'
+    colors = [purple, yellow, blue]
+    variables_names = [r'$q_t$', r'$h_t$', r'$s_t$', r'$s_{t+1}$', r'$r_{t+1}$', r'$r^{delay}_{t+1}$']
+    variables = ['q', 'h_t', 's_t', 's_t+1', 'r_t+1', 'r_d_t+1']
+
+    image_format = ['png']
+    for im in range(len(image_format)):
+        for policy in range(len(policies)):
+            if not os.path.exists(output_folder + '/' + feature + '/' + image_format[im] + '/' + policies[policy]):
+                os.makedirs(output_folder + '/' + feature + '/' + image_format[im] + '/' + policies[policy])
+    # this generates 8 plots one for each irrigation district:
+    for ir in range(len(irr_d)):
+        irr_plots(input_folder, target_input_folder, output_folder, feature, ir, irr_d, irr_index, policies, months,
+                  label_policy, n_months, n_years, colors)
+
+    # this a summary of the delta releases:
+    mef_plots(input_folder, output_folder, label_policy, delta_release_balance, feature, policies, n_years, n_months,
+              delta_target, colors, months, title, target_input_folder)
+
+    v = 4  # to print only releases across all reservoirs:
+    # for v in range(len(variables)-1): to print all summary figures:
+    for p in range(len(policies)):
+        fig = plt.figure()
+        for r in range(len(reservoirs)):
+            summary_plot(v, p, r, fig, input_folder, output_folder, feature, policies, variables, label_policy,
+                         reservoirs, res_names, months, n_years, n_months)
+
+def irr_plot_quantities():
+    plt.rcParams["font.family"] = "Myriad Pro"
+    sns.set_style("whitegrid")
+
+    input_folder = '../storage_release/'
+    # input_folder_objs='../for_plots/'
+    target_input_folder = '../data/'
+    output_folder = '../plots/'
+    delta_target = np.loadtxt(target_input_folder + 'MEF_delta.txt')
+    # n_objs=3
+
+    # copy here..
+    #####################################
+    feature = 'irr_policy_simulation'
+    # reservoirs=['itt','kgu','kgl','ka','bg','dg','cb','mn']
+    reservoirs = ['itt', 'kgu', 'kgl', 'ka', 'cb']
+    title = '5_res_wKGL'
+    # input_file='Zambezi_'+title+'.reference'  #'.reference'change file_name
+
+    # data= np.loadtxt('../parallel/sets/'+feature+'/'+input_file, skiprows=0+1+2-1)
+    delta_release_balance = '\n('r'$r_{CB}+Q_{Shire}-r_{Irrd7}-r_{Irrd8}-r_{Irrd9}$)'
+    # res_names=['Itezhitezhi','Kafue G. Upper','Kafue G. Lower','Kariba','Batoka Gorge','Devil\'s Gorge','Cahora Bassa', 'Mphanda Nkuwa']
+    res_names = ['Itezhitezhi', 'Kafue G. Upper', 'Kafue G. Lower', 'Kariba', 'Cahora Bassa']
+    #####copy the segment above#########
+
+    policies = ['best_hydro', 'best_env', 'best_irr','best_irr2', 'best_irr3', 'best_irr4', 'best_irr5', 'best_irr6', 'best_irr7', 'best_irr8', 'best_irr9']
+    irr_index = ['2', '3', '4', '5', '6', '7', '8', '9'] #*******************#
     irr_d = ['Irrigation District 2', 'Irrigation District 3', 'Irrigation District 4', 'Irrigation District 5',
              'Irrigation District 6', 'Irrigation District 7', 'Irrigation District 8', 'Irrigation District 9']
     label_policy = ['Best Hydropower', 'Best Environment', 'Best Irrigation', 'Target Demand']
